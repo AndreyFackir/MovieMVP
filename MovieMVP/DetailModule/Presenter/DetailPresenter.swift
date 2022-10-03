a1c4b724fc17f291237cc4fae7a1a52f02cec997
AndreyFackir <104596263+AndreyFackir@users.noreply.github.com>
2022-10-03 21:03:27 +0300

    Some refactoring

--- MovieMVP/DetailModule/Presenter/DetailPresenter.swift
+++ MovieMVP/DetailModule/Presenter/DetailPresenter.swift
@@ -100,7 +100,6 @@ class DetailPresenter: DetailViewPresenterProtocol {
   
   func saveToFavourites() {
     let favourites = Favourites()
-   
     if movie == nil {
       favourites.id = serial?.id ?? 0
       favourites.favouriteMovieImageUrl = getSerialAndMovieString()
